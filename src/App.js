ad4a7b03b9f805d0433fb2d26fa36ecf0ecf518b
Ayon Mandal <ayonmandal@Ayons-MacBook-Pro.local>
2022-10-21 13:06:15 +0530

    Basic Card Componet Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container, Stack } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import BudgetCard from "./components/BudgetCard";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container className="my-2">
+      <Stack direction="horizontal" gap={2} className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <BudgetCard name="Entertainment" amount={1100} maxAmount={1000} />
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,45 @@
+import Button from "react-bootstrap/Button";
+import React from "react";
+import { Card, ProgressBar, Stack } from "react-bootstrap";
+import { currencyFormatter } from "../utils/CurrencyFormatter";
+
+const BudgetCard = (props) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+          <div className="me-2">{props.name}</div>
+          <div className="d-flex align-items-baseline">
+            {currencyFormatter.format(props.amount)} /{" "}
+            <span className="text-muted fs-6">
+              {currencyFormatter.format(props.maxAmount)}
+            </span>
+          </div>
+        </Card.Title>
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVariant(props.amount, props.maxAmount)}
+          min={0}
+          max={props.maxAmount}
+          now={props.amount}
+        />
+        <Stack direction="horizontal" gap={2} className="mt-4">
+          <Button variant="outline-primary" className="ms-auto">
+            Add Expense
+          </Button>
+          <Button variant="outline-secondary">View Expenses</Button>
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+};
+
+function getProgressBarVariant(amount, maxAmount) {
+  const ratio = amount / maxAmount;
+  if (ratio <= 0.25) return "success";
+  if (ratio <= 0.5) return "info";
+  if (ratio <= 0.75) return "warning";
+  return "danger";
+}
+
+export default BudgetCard;

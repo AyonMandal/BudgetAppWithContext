3070225acae5280178e0b599dcdc0a65eb8d5f33
Ayon Mandal <ayonmandal@Ayons-MacBook-Pro.local>
2022-11-01 10:52:20 +0530

    Add Budget Modal Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { Container, Stack } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
+import { useState } from "react";
 
 const App = () => {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  function hideAddBudgetModal() {
+    setShowAddBudgetModal(false);
+  }
   return (
-    <Container className="my-2">
-      <Stack direction="horizontal" gap={2} className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary">Add Expense</Button>
-      </Stack>
-      <BudgetCard
-        name="Entertainment"
-        amount={1750}
-        maxAmount={2400}
-        bgGray={true}
+    <>
+      <Container className="my-2">
+        <Stack direction="horizontal" gap={2} className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <BudgetCard
+          name="Entertainment"
+          amount={1750}
+          maxAmount={2400}
+          bgGray={true}
+        />
+      </Container>
+      <AddBudgetModal
+        showModal={showAddBudgetModal}
+        hideModal={hideAddBudgetModal}
       />
-    </Container>
+    </>
   );
 };
 

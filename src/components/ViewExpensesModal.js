96d9e9d27e7421f73463f6635a07c3d00fe12bee
Ayon Mandal <ayonmandal@192.168.1.2>
2022-11-15 23:53:19 +0530

    Basic functionalty completed

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Stack } from "react-bootstrap";
 import { useBudgets } from "../context/BudgetContext";
+import { currencyFormatter } from "../utils/CurrencyFormatter";
 
 const ViewExpensesModal = (props) => {
-  const { budgets, expenses } = useBudgets();
+  const { budgets, getBudgetExpenses } = useBudgets();
+  const expensesList = getBudgetExpenses(props.passBudgetId);
   const getBudgetDetails = budgets.filter(
     (budget) => props.passBudgetId === budget.id
   )[0];
@@ -15,7 +17,24 @@ const ViewExpensesModal = (props) => {
           {getBudgetDetails ? getBudgetDetails.name : "Uncategorized"} Expenses
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Body>
+        <Stack direction="vertical" gap={3}>
+          {expensesList.map((expense) => (
+            <>
+              <Stack direction="horizontal" gap={2} key={expense.id}>
+                <div className="me-auto fs-6">{expense.description}</div>
+                <div className="fs-5">
+                  {" "}
+                  {currencyFormatter.format(expense.amount)}
+                </div>
+                <Button size="sm" variant="outline-danger">
+                  &times;
+                </Button>
+              </Stack>
+            </>
+          ))}
+        </Stack>
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="danger" onClick={() => console.log(getBudgetDetails)}>
           Delete all expenses
